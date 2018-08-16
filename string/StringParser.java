343d845d01d2e73b8d58e6e7da4e0d6e7e456e9f
Sandeep Tiwari <sandeep15mca@gmail.com>
2018-08-16 11:54:57 +0530

    add test cases of string parser

--- string/StringParser.java
+++ string/StringParser.java
@@ -10,13 +10,18 @@
 
 public class StringParser{
 
- public boolean trailingZeroAndSpace(String a, String String b){
- 
+ public boolean trailingZeroAndSpace(String input, String String output){
+       input = input.replaceFirst("^0*", "").trim();
+       return input.equals(output.trim());
  }
 
  public static void main(String...args){
 	StringParser parser = new StringParser();
-	System.out.println(parser.trailingZeroAndSpace("0010000", "  10 "));
+       //boolean result = parser.trailingZeroAndSpace("0001", "1    ");
+       //boolean result = parser.trailingZeroAndSpace("1450", "1450   ");
+       //boolean result = parser.trailingZeroAndSpace("0010001 ", "10001    ");
+       boolean result = parser.trailingZeroAndSpace("101023", "101024    ");
+       System.out.print("Result :: "+result);
  }
 
 }
