cf791496ad492f393e9cdef8f6c4bf7439c3ebe3
Sandeep Tiwari <sandeep15mca@gmail.com>
2019-02-13 15:40:43 +0530

    Update BSTTraversing.java
    Added morrisTraversal implementation

--- BSTTraversing.java
+++ BSTTraversing.java
@@ -67,6 +67,54 @@ private static void inorderTraverse(TreeNode bsTree ){
 
         }
     }
+    
+    /**
+     * 1. Initialize current as root
+     * 2. While current is not NULL
+     *    If current does not have left child
+     *       a) Print current’s data
+     *       b) Go to the right, i.e., current = current->right
+     *    Else
+     *       a) Make current as right child of the rightmost
+     *          node in current's left subtree
+     *       b) Go to this left child, i.e., current = current->left
+     */
+    public void morrisTraversal(Root root){
+        Root current, pre;
+
+        if (root == null)
+            return;
+
+        current = root;
+        while (current != null) {
+            if (current.left == null) {
+                System.out.print(current.data + " ");
+                current = current.right;
+            }
+            else {
+                /* Find the inorder predecessor of current */
+                pre = current.left;
+                while (pre.right != null && pre.right != current)
+                    pre = pre.right;
+
+                /* Make current as right child of its inorder predecessor */
+                if (pre.right == null) {
+                    pre.right = current;
+                    current = current.left;
+                }
+
+                /* Revert the changes made in if part to restore the
+                    original tree i.e., fix the right child of predecssor*/
+                else {
+                    pre.right = null;
+                    System.out.print(current.data + " ");
+                    current = current.right;
+                } /* End of if condition pre->right == NULL */
+
+            } /* End of if condition current->left == NULL*/
+
+        } /* End of while */
+    }
 	
 	//inOrder traversing by recursively
 	public void inOrder(TreeNode root) {  
